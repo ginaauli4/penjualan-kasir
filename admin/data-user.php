d8ebcc530b92f2f0c384553ee83f5dbee8439a24
ginaauli4 <ginaaul120@gmail.com>
2024-03-06 07:54:14 +0700

    commit lagi

--- admin/data-user.php
+++ admin/data-user.php
@@ -13,7 +13,7 @@
         <meta name="author"
               content="">
 
-        <title>Dashboard Admin</title>
+        <title>Dashboard User</title>
 
         <!-- Custom fonts for this template-->
         <link href="../vendor/fontawesome-free/css/all.min.css"
